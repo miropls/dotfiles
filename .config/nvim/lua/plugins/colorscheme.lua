f87ed167184c1f17a229238edcda55e6688ace79
Miro Paintola <miro.paintola@gmail.com>
2024-12-14 01:31:03 +0200

    switch to tokyonight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
-	"catppuccin/nvim",
+	"folke/tokyonight.nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("catppuccin-mocha")
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
