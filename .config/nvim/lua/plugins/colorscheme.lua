dc61afff054d9add7e18ae605bcf9267f75d4e35
Miro Paintola <miro.paintola@gmail.com>
2025-01-18 22:48:16 +0200

    starship update, go back to transparent term/nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
 	"catppuccin/nvim",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
