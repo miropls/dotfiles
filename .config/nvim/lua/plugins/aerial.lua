5dbefa54ea51dd4dc1f479a43a5b1a0a79de14fa
Miro Paintola <miro.paintola@gmail.com>
2025-03-27 13:47:41 +0200

    ei näitä kukkaan lue

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -6,8 +6,8 @@ return {
 			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
 			on_attach = function(bufnr)
 				-- Jump forwards/backwards with '{' and '}'
-				vim.keymap.set("n", "<C-l>", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-				vim.keymap.set("n", "<C-u>", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				-- vim.keymap.set("n", "<C-l>", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				-- vim.keymap.set("n", "<C-u>", "<cmd>AerialNext<CR>", { buffer = bufnr })
 			end,
 		})
 		-- You probably also want to set a keymap to toggle aerial
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,66 +1,23 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "echasnovski/mini.nvim" },
 	config = function()
-		local color = require("lackluster.color")
-		local color_special = require("lackluster.color-special")
-
-		local lualine_theme = {
-			normal = {
-				a = { bg = color_special.statusline, fg = color.green, gui = "bold" },
-				b = { bg = color_special.statusline, fg = color.gray7 },
-				c = { bg = color_special.statusline, fg = color.gray7 },
-			},
-			insert = {
-				a = { bg = color.green, fg = color.gray2, gui = "bold" },
-				b = { bg = color_special.statusline, fg = color.gray7 },
-				c = { bg = color_special.statusline, fg = color.gray7 },
-			},
-			command = {
-				a = { bg = color.blue, fg = color.gray2, gui = "bold" },
-				b = { bg = color_special.statusline, fg = color.gray7 },
-				c = { bg = color_special.statusline, fg = color.gray7 },
-			},
-			visual = {
-				a = { bg = color.orange, fg = color.gray2, gui = "bold" },
-				b = { bg = color_special.statusline, fg = color.gray7 },
-				c = { bg = color_special.statusline, fg = color.gray7 },
-			},
-			replace = {
-				a = { bg = color.gray9, fg = color.black, gui = "bold" },
-				b = { bg = color_special.statusline, fg = color.gray7 },
-				c = { bg = color_special.statusline, fg = color.gray7 },
-			},
-			inactive = {
-				a = { bg = color.gray1, fg = color.gray4, gui = "bold" },
-				b = { bg = color.gray1, fg = color.gray4 },
-				c = { bg = color.gray1, fg = color.gray4 },
-			},
-		}
-
-		require("nvim-web-devicons").setup({
-			color_icons = false,
-			override = {
-				["default_icon"] = {
-					color = color.green,
-					name = "Default",
-				},
-			},
-		})
+		require("mini.icons").setup()
+		require("mini.icons").mock_nvim_web_devicons()
 
 		require("lualine").setup({
 			options = {
-				theme = lualine_theme,
+				theme = "auto",
 				component_separators = "|",
-				section_separators = "",
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = { "mode" },
+				lualine_a = { { "mode", separator = { left = "" }, left_padding = 4 } },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "filename" },
 				lualine_x = { { "overseer", colored = true } },
 				lualine_y = { "encoding", "fileformat", "filetype" },
-				lualine_z = { "location" },
+				lualine_z = { { "location", separator = { right = "" }, right_padding = 4 } },
 			},
 		})
 	end,
