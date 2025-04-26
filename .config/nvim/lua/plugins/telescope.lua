0720b6a0a872424ef6115da9784a1c8f4ac8d641
Miro Paintola <miro@paintola.tech>
2025-04-26 21:33:32 +0300

    fix for broken conf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,66 +1,54 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
-	keys = {
-		{ "<leader>ff", require("telescope.builtin").find_files, desc = "Files" },
-		{ "<leader>fg", require("telescope.builtin").git_files, desc = "Git files" },
-		{
-			"<leader>c",
-			function()
-				require("telescope.builtin").find_files({
-					cwd = vim.fn.stdpath("config"),
-				})
-			end,
-			desc = "Open Neovim config",
-		},
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+    "nvim-telescope/telescope-ui-select.nvim",
+  },
+  config = function()
+    local config = require("telescope.config")
+    local vimgrep_arguments = { unpack(config.values.vimgrep_arguments) }
 
-		{
-			"<leader>p",
-			function()
-				require("telescope.builtin").live_grep(require("telescope.themes"))
-			end,
-			desc = "Fuzzy search",
-		},
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
 
-		-- git
-		{ "<leader>ggc", require("telescope.builtin").git_commits, desc = "Commits" },
-		{ "<leader>ggb", require("telescope.builtin").git_branches, desc = "Branches" },
-		{ "<leader>ggs", require("telescope.builtin").git_status, desc = "Status" },
-		{ "<leader>ggt", require("telescope.builtin").git_stash, desc = "Stash" },
-	},
-	config = function()
-		local config = require("telescope.config")
-		local vimgrep_arguments = { unpack(config.values.vimgrep_arguments) }
+    require("telescope").setup({
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+        find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({}),
+        },
+      },
+    })
 
-		table.insert(vimgrep_arguments, "--hidden")
-		table.insert(vimgrep_arguments, "--glob")
-		table.insert(vimgrep_arguments, "!**/.git/*")
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("ui-select")
 
-		require("telescope").setup({
-			defaults = {
-				vimgrep_arguments = vimgrep_arguments,
-			},
-			pickers = {
-				find_files = {
-					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-				},
-			},
-			extensions = {
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({}),
-				},
-			},
-		})
+    vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Files" })
+    vim.keymap.set("n", "<leader>fg", require("telescope.builtin").git_files, { desc = "Git files" })
+    vim.keymap.set("n", "<leader>c",
+      function() require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config"), }) end,
+      { desc = "Open Neovim config", })
 
-		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("ui-select")
-	end,
+    vim.keymap.set("n", "<leader>p", function() require("telescope.builtin").live_grep(require("telescope.themes")) end,
+      { desc = "Fuzzy search", })
+
+    -- git
+    vim.keymap.set("n", "<leader>ggc", require("telescope.builtin").git_commits, { desc = "Commits" })
+    vim.keymap.set("n", "<leader>ggb", require("telescope.builtin").git_branches, { desc = "Branches" })
+    vim.keymap.set("n", "<leader>ggs", require("telescope.builtin").git_status, { desc = "Status" })
+    vim.keymap.set("n", "<leader>ggt", require("telescope.builtin").git_stash, { desc = "Stash" })
+  end,
 }
