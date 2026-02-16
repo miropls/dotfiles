0878b48ae98c7b5eb5c2a0ebf316e0f2e0280306
Miro Paintola <miro.paintola@gmail.com>
2026-02-16 11:16:15 +0200

    add git push and pull abbriviations

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -5,3 +5,5 @@ abbr -a -- zel zellij
 abbr -a -- md mkdir
 abbr -a -- ... ../../
 abbr -a -- .... ../../../
+abbr -a -- gp 'git pull'
+abbr -a -- gP 'git push -u origin DHMA-1110/migrate-secrets-to-eas --force-with-lease'
